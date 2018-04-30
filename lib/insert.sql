79d6646f1124f02b71e0a24845cb9901e471ec2e
Kathy Hudson <khitx@icloud.com>
2018-04-30 21:22:35 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,5 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('george', 15, 'M', 'brown', 'calm', 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('doug', 22 'M', 'black', 'mean', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('doug', 22, 'M', 'black', 'mean', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('wayne', 1, 'M', 'white', 'calm', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('beary', 32, 'F', 'brown', 'prissy', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('liz', 5, 'F', 'black', 'calm', 1);
