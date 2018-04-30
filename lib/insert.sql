32734c286f36f9fa72dccb5373cbabff9899cc87
Kathy Hudson <khitx@icloud.com>
2018-04-30 21:22:15 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('george', 15, 'M', 'brown', 'calm', 1)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('doug', 22 'M', 'black', 'mean', 1)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('wayne', 1, 'M', 'white', 'calm', 1)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('beary', 32, 'F', 'brown', 'prissy', 1)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('liz', 5, 'F', 'black', 'calm', 1)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('alex', 7, 'F', 'white', 'fun', 1)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('kate', 18, 'F', 'brown', 'calm', 1)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('george', 4, 'M', 'brown', 'calm', 1)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('george', 15, 'M', 'brown', 'calm', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('doug', 22 'M', 'black', 'mean', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('wayne', 1, 'M', 'white', 'calm', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('beary', 32, 'F', 'brown', 'prissy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('liz', 5, 'F', 'black', 'calm', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('alex', 7, 'F', 'white', 'fun', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('kate', 18, 'F', 'brown', 'calm', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('george', 4, 'M', 'brown', 'calm', 1);
